0c1c847a9c0016d70d503f579c8196e87b1e79ff
Austin Richardson <richardson.austin35@gmail.com>
2020-02-10 11:27:07 -0800

    Integration ---

--- App.js
+++ App.js
@@ -1,35 +1,58 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigator from './routes/drawer';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import React, { Component } from 'react';
+import AdminLogin from './screens/AdminLogin';
+import AdminMain from './screens/AdminMain';
+import CustomerLogin from './screens/CustomerLogin';
+import {UserHome} from './screens/userHome';
+//import Navigator from 'routes-drawer';
+//import About from './screens/about';
+import DriverLogin from './screens/DriverLogin';
+import driverHome from './screens/driverHome';
+import UserRoles from './screens/UserRoles';
+import AddDriver from './screens/AddDriver';
+import RemoveDriver from './screens/RemoveDriver';
+import CustomerInfo from './screens/CustomerInfo';
+import DriverInfo from './screens/DriverInfo';
+import ModifyDriver from './screens/ModifyDriver';
+import ForgotPassword from './screens/ForgotPassword';
+import CustomerLoginAsGuest from './screens/CustomerLoginAsGuest';
+import ApiKeys from './shared/ApiKeys';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import * as firebase from 'firebase';
+import '@firebase/firestore';
 
-const getFonts = () => Font.loadAsync({
-  'arvo-regular': require('./assets/fonts/Arvo-Regular.ttf'),
-  'arvo-bold': require('./assets/fonts/Arvo-Bold.ttf'),
-});
 
-export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-  if(fontsLoaded){
-    return (
-      <Navigator />
-    );
-  } else {
-    return(
-      <AppLoading
-        startAsync = {getFonts}
-        onFinish = {() => setFontsLoaded(true)}
-      />
-    )
-  }
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(ApiKeys.firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppSwitchNavigator = createSwitchNavigator({
+  URoles: UserRoles,
+  CLogin: CustomerLogin,
+  CGuest: CustomerLoginAsGuest,
+  CMain: UserHome,
+  CInfo: CustomerInfo,
+  DLogin: DriverLogin,
+  DMain: driverHome,
+  DInfo: DriverInfo,
+  DModify: ModifyDriver,
+  ALogin: AdminLogin,
+  AMain: AdminMain,
+  AddDriver: AddDriver,
+  RemoveDriver: RemoveDriver,
+  ForgotPassword: ForgotPassword
 });
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+
+export default class App extends React.Component {
+  render() {
+    return <AppNavigator /> 
+  } 
+}
+
+
+
+
+
+
