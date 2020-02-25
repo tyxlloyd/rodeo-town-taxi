4be7dd809ece428c5f147d60dfafdc2c3c87040c
Stephen <stephen.stengel@cwu.edu>
2020-02-25 13:50:14 -0800

    Fixed the picture and stuff.

--- screens/UserRoles.js
+++ screens/UserRoles.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Alert, Image } from 'react-native';
 import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
 import * as firebase from 'firebase';
-
+import * as Permissions from 'expo-permissions';
 
 class UserRoles extends React.Component {
   TitlePicker() {
@@ -27,6 +27,52 @@ class UserRoles extends React.Component {
 
     }
   }
+
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      allowLocation: false,
+    }
+
+    this._getPermissionAsync();
+
+  }
+
+
+
+  _getPermissionAsync = async () => {
+    try {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+        if (status !== 'granted') {
+            this.setState({ allowLocation: false });
+        }
+        else {
+          this.setState({ allowLocation: true });
+        }
+    }
+
+    catch (e) {
+        console.log('_getPermissionAsyncError: ' + e)
+    }
+  }
+
+
+  checkIfPermissionGranted(pageToNavigateTo){
+    if(this.state.allowLocation == true){
+      this.props.navigation.navigate(pageToNavigateTo);
+    }
+    else {
+      Alert.alert("Please enable location access",
+        "We need your location to use the map. Please go into your phone's settings menu to enable location services for Rodeo Town Taxi.");
+    }
+  }
+
+
+
+
   render() {
     return (
       <Container style={styles.container}>
