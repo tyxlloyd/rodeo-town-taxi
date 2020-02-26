961c17215a39d4faef2f25d7805ec87120edf11e
tyxlloyd <tyxlloyd@yahoo.ca>
2020-02-25 17:48:44 -0800

    Fixed image on UserRoles

--- screens/UserRoles.js
+++ screens/UserRoles.js
@@ -85,7 +85,7 @@ class UserRoles extends React.Component {
           <View style={styles.imageContainer}>
             <Image
               style={{ width: 310, height: 310 }}
-              source={{ uri: 'https://lh3.googleusercontent.com/nkvFLUHasEeoje78Ng9wSbExZRYIMt3XQsKMjwJFRWovKtF4NwDEUvjh7rShPmWOXGmgIrHs0aflfXgkWYdU4pmTgWmMT3nApMrX-3DQlAMTq0sfrCn_PpOZPCVV3TiGc3M4VPrXO7L9Wu0Mi2KyOP8SQc2zsA7SfkYMzFCuQhKnQ2_8a0ogoztyd1L2bTHHaHX9Cru8RlBvdmQUrzYWXEDkGy2FjanPRsyXiBIetKZnFz1plRV6qg1TC0pJDDEByuR7zooDGcVLIz-ATqt720p60MFEbwF_PSjxx9OlnNJahI9SXATraXOYJ4rtavI2ZDR3Y8rJrRDMznAXDMvqShxI2keKM3IbG6XeWdMnTjy7fKSLFbfsLiXLNH0IxsaMwn3Fz5aj0i1oQntGiYwCug4btbJi67wb2vZxX4iWR2VQnjhPmqauiGPPip17Vn-FHkaZKgt3jHkZ9amCns38ilt3DF8JG-Maq5imA67cNeKvncGnVYyDOWI1X8gYEfLdd9m2oWgFALnAjJT7p676SdGL4skT8gPLilxVTAJQmHG1C_uGUrAco571GTAN9qiOGnxiS06zjcyCnYHxYB-3Ff-QwJhzVUB0LUsRoDFZHr7Ha6q4eYTNO5hVHpi_mKeSLNX1OFUprND3MuQU_yJgVgeih7f_oVOUYuXBFjAgze9-_G8QCNrt=w744-h945-no' }}
+              source={require('../assets/RTTIconTransparent.png')}
             />
           </View>
 
